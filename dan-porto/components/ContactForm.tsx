54034b6b6e7f84c8d1981474341ff12256cb786b
Didan Fariz <112478419+didanfarizz@users.noreply.github.com>
2025-03-06 20:03:36 +0700

    .

--- dan-porto/components/ContactForm.tsx
+++ dan-porto/components/ContactForm.tsx
@@ -4,11 +4,11 @@ const ContactForm = () => {
   return (
     <div>
       <div className="">
-        <form action="">
-          <input type="name" id='name' placeholder='Name' className='bg-transparent border-b-2 border-white' />
-          <input type="email" id='email' placeholder='Email' className='bg-transparent border-b-2 border-white' />
-          <input type="subject" id='subject' placeholder='Subject' className='bg-transparent border-b-2 border-white' />
-          <textarea name="message" id="message" rows={10} placeholder='Message' className='bg-transparent border-b-2 border-white'></textarea>
+        <form action="" className='flex flex-col p-10 space-y-5'>
+          <input type="name" id='name' placeholder='Name' className='bg-white p-3 border-b-2 border-white' />
+          <input type="email" id='email' placeholder='Email' className='bg-white p-3 border-b-2 border-white' />
+          <input type="subject" id='subject' placeholder='Subject' className='bg-white p-3 border-b-2 border-white' />
+          <textarea name="message" id="message" rows={10} placeholder='Message' className='bg-white p-3 border-b-2 border-white'></textarea>
         </form>
       </div>
     </div>
